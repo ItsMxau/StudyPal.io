f8032d117de43173c2441fc3be330c481d61e751
ItsMxau <maurice.thiemann07@gmail.com>
2024-04-06 15:43:46 +0800

    Initial Commit

---
+++ StudyPal/StudyPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPalApp.swift
+//  StudyPal
+//
+//  Created by CIS Maurice  on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct StudyPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
